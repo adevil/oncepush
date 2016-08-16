248e9a59d31efc6f41421b02ffa02772fb87fd9c
soliars <2575392997@qq.com>
2016-08-16 11:16:41 +0800

    备注无用代码,修改sina groupid

--- oncepush/src/main/java/com/duofa/oncepush/cloud/ucloud/util/UcloudUtil.java
+++ oncepush/src/main/java/com/duofa/oncepush/cloud/ucloud/util/UcloudUtil.java
@@ -30,40 +30,40 @@ public class UcloudUtil {
      * @throws UnsupportedEncodingException
      * @throws URISyntaxException
      */
-    @Deprecated
-    public static String getSignature(String url, Map<String, String> params, String encoding, String privateKey)
-            throws UnsupportedEncodingException, URISyntaxException {
-
-        //判断参数是否正确
-        if (params == null || params.size() == 0) {
-            LOGGER.error("get signature error : params is null :{params:{url:{},params:{},encoding:{},privateKey:secret}",
-                    url,JsonUtil.beanToJson(params),encoding);
-            throw new CloudApiCallException();
-        }
-
-
-        //对请求参数进行URL编码
-        for (Map.Entry<String, String> entry : params.entrySet()) {
-            params.put(entry.getKey(), URLEncoder.encode(entry.getValue(), encoding));
-        }
-
-        List<Map.Entry<String, String>> sortList = new ArrayList<Map.Entry<String, String>>(params.entrySet());
-
-        //将请求参数按照名进行升序排列
-        Collections.sort(sortList, new Comparator<Map.Entry<String, String>>() {
-            public int compare(Map.Entry<String, String> o1, Map.Entry<String, String> o2) {
-                Map.Entry obj1 = (Map.Entry) o1;
-                Map.Entry obj2 = (Map.Entry) o2;
-                return (obj1.getKey()).toString().compareTo(obj2.getKey().toString());
-            }
-        });
-
-        //生成http请求以及签名字符串
-        URIBuilder uriBuilder = new URIBuilder(url);
-        for (Map.Entry<String, String> entry : sortList) {
-            uriBuilder.setParameter(entry.getKey(), entry.getValue());
-        }
-        String linkUrl = uriBuilder.build().toString() + privateKey;
-        return DigestUtils.sha1Hex(linkUrl);
-    }
+//    @Deprecated
+//    public static String getSignature(String url, Map<String, String> params, String encoding, String privateKey)
+//            throws UnsupportedEncodingException, URISyntaxException {
+//
+//        //判断参数是否正确
+//        if (params == null || params.size() == 0) {
+//            LOGGER.error("get signature error : params is null :{params:{url:{},params:{},encoding:{},privateKey:secret}",
+//                    url,JsonUtil.beanToJson(params),encoding);
+//            throw new CloudApiCallException();
+//        }
+//
+//
+//        //对请求参数进行URL编码
+//        for (Map.Entry<String, String> entry : params.entrySet()) {
+//            params.put(entry.getKey(), URLEncoder.encode(entry.getValue(), encoding));
+//        }
+//
+//        List<Map.Entry<String, String>> sortList = new ArrayList<Map.Entry<String, String>>(params.entrySet());
+//
+//        //将请求参数按照名进行升序排列
+//        Collections.sort(sortList, new Comparator<Map.Entry<String, String>>() {
+//            public int compare(Map.Entry<String, String> o1, Map.Entry<String, String> o2) {
+//                Map.Entry obj1 = (Map.Entry) o1;
+//                Map.Entry obj2 = (Map.Entry) o2;
+//                return (obj1.getKey()).toString().compareTo(obj2.getKey().toString());
+//            }
+//        });
+//
+//        //生成http请求以及签名字符串
+//        URIBuilder uriBuilder = new URIBuilder(url);
+//        for (Map.Entry<String, String> entry : sortList) {
+//            uriBuilder.setParameter(entry.getKey(), entry.getValue());
+//        }
+//        String linkUrl = uriBuilder.build().toString() + privateKey;
+//        return DigestUtils.sha1Hex(linkUrl);
+//    }
 }
