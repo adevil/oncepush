c17c0682c079806b41039dae5909f8a7f2755c18
soliars <2575392997@qq.com>
2016-08-16 10:32:48 +0800

    db

---
+++ duofa_sina/src/main/java/com/duofa/sina/oauth2/OAuth2Api.java
@@ -0,0 +1,12 @@
+package com.duofa.sina.oauth2;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OAuth2Api {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(OAuth2Api.class);
+
+	public static getAuth2
+}
